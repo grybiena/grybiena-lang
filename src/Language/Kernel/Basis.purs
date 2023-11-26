9e56de922df3c128d4cc45eb11e8198a09be4a7a
f <f@grybiena.com>
2023-11-26 13:33:59 +0100

    extend basis

--- src/Language/Kernel/Basis.purs
+++ src/Language/Kernel/Basis.purs
@@ -2,6 +2,7 @@ module Language.Kernel.Basis where
 
 import Prelude
 
+import Control.Lazy (fix)
 import Language.Native.Unsafe (UnsafeNative(..))
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -10,6 +11,9 @@ basis ::
   { "S" :: UnsafeNative "forall a b c. (a -> b -> c) -> (a -> b) -> a -> c" 
   , "K" :: UnsafeNative "forall a b. a -> b -> a" 
   , "I" :: UnsafeNative "forall a. a -> a" 
+  , "C" :: UnsafeNative "forall a b c. (b -> a -> c) -> a -> b -> c"
+  , "B" :: UnsafeNative "forall a b c. (b -> c) -> (a -> b) -> a -> c"
+  , "Y" :: UnsafeNative "forall a b. ((a -> b) -> (a -> b)) -> (a -> b)"
   }
 basis = 
   { "S": UnsafeNative
@@ -24,7 +28,17 @@ basis =
            let exp :: forall a. a -> a
                exp = identity
             in unsafeCoerce exp
+  , "C": UnsafeNative
+           let exp :: forall a b c. (b -> a -> c) -> a -> b -> c
+               exp x y z = (x z) y
+            in unsafeCoerce exp 
+  , "B": UnsafeNative
+           let exp :: forall a b c. (b -> c) -> (a -> b) -> a -> c
+               exp x y z = x (y z)
+            in unsafeCoerce exp
+  , "Y": UnsafeNative
+           let exp :: forall a b. ((a -> b) -> (a -> b)) -> (a -> b)
+               exp = fix
+            in unsafeCoerce exp
   }
-
-
 
