4b4d65aeececd598c28ae801014da80c56bd4e9e
f <f@grybiena.com>
2023-11-27 15:30:25 +0100

    refactor tarjan into RWS for clarity

--- src/Data/Graph/SCC.purs
+++ src/Data/Graph/SCC.purs
@@ -3,15 +3,25 @@ module Data.Graph.SCC (scc) where
 -- based on a Haskell implementation of Tarjan's SCC algorithm by @paf31
 -- https://github.com/purescript-deprecated/purescript-maps/commit/c976f02e53dd22bf12418a034eea0e971bb09cb5
 
+-- refactored to be clearly isomorphic to Tarjan's algorithm as described here
+-- https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm 
+
 import Prelude
 
-import Data.Foldable (elem, foldl)
+import Control.Monad.RWS (RWS, execRWS)
+import Control.Monad.Reader (class MonadReader, ask)
+import Control.Monad.State (class MonadState, modify_)
+import Control.Monad.State.Class (gets)
+import Data.Foldable (elem)
 import Data.Graph.Edge (Edge(..))
-import Data.List (List(..), fold, (:))
+import Data.List (List(..), filter, fold, (:))
+import Data.List as List
 import Data.Map (Map)
 import Data.Map as Map
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), isNothing)
 import Data.Topos.Pointed (points)
+import Data.Traversable (sequence_, traverse_)
+import Data.Tuple (fst)
 import Data.Tuple.Nested ((/\))
 
 type Index = Int
@@ -26,8 +36,29 @@ type SCCState v =
 
 scc :: forall v. Ord v => List (Edge v) -> List (List v)
 scc g = 
-  let st = foldl (scc' g) initialSCCState (fold (points <$> g))
+  let f :: RWS (List (Edge v)) Unit (SCCState v) Unit
+      f = (sequence_ $ map strongConnect (List.fromFoldable $ fold (points <$> g)))
+      st = fst $ execRWS f g initialSCCState
   in st.components
+ 
+strongConnect :: forall v m.
+          MonadState (SCCState v) m
+       => MonadReader (List (Edge v)) m
+       => Ord v => v -> m Unit 
+strongConnect v = do
+  whenIndexUndefined v do
+    initVertex v
+    onSuccessors v $ \w -> do
+      notVisited <- isNothing <$> indexOf w
+      if notVisited 
+        then do
+          strongConnect w
+          updateLowLink v w
+        else do
+          whenOnStack w do
+            altUpdateLowLink v w
+    whenIsRootNode v do
+      generateSCC v
 
 initialSCCState :: forall v. SCCState v
 initialSCCState =
@@ -38,47 +69,68 @@ initialSCCState =
   , components       : mempty
   }
 
-
-initVertex :: forall v. Ord v => SCCState v -> v -> SCCState v
-initVertex st v =
+initVertex :: forall v m. MonadState (SCCState v) m => Ord v => v -> m Unit
+initVertex v = modify_ $ \st ->
   st { indexMap = Map.insert v st.index st.indexMap
      , lowlinkMap = Map.insert v st.index st.lowlinkMap
      , index = st.index + 1
      , path = v:st.path
      }
 
-onSuccessors :: forall v. Ord v => List (Edge v) -> SCCState v -> v -> (SCCState v -> v -> SCCState v) -> SCCState v
-onSuccessors es st v f = foldl (\st' (Edge (v' /\ w)) -> if v == v' then f st' w else st') st es
+indexOf :: forall v m. MonadState (SCCState v) m => Ord v => v -> m (Maybe Index)
+indexOf v = gets (\st -> Map.lookup v st.indexMap)
 
+lowLinkOf :: forall v m. MonadState (SCCState v) m => Ord v => v -> m (Maybe Index)
+lowLinkOf v = gets (\st -> Map.lookup v st.lowlinkMap)
 
-scc' :: forall v. Ord v => List (Edge v) -> SCCState v -> v -> SCCState v
-scc' es sccst v | Map.lookup v sccst.indexMap == Nothing = 
-  let indexOf   st' v' = Map.lookup v' st'.indexMap
-      lowlinkOf st' v' = Map.lookup v' st'.lowlinkMap
+onSuccessors :: forall v m.
+                 MonadState (SCCState v) m
+              => MonadReader (List (Edge v)) m
+              => Ord v => v -> (v -> m Unit) -> m Unit 
+onSuccessors v f = do
+  es <- ask
+  let isSuccessor (Edge (v' /\ _)) = v' == v
+      successor (Edge (_ /\ w)) = w
+  traverse_ f (successor <$> (filter isSuccessor es)) 
 
-      st1 = initVertex sccst v 
+whenIndexUndefined :: forall v m. MonadState (SCCState v) m => Ord v => v -> m Unit -> m Unit 
+whenIndexUndefined v f = do
+  i <- indexOf v
+  case i of
+    Nothing -> f
+    _ -> pure unit
 
-      st2 = onSuccessors es st1 v (\st w ->
-              case indexOf st w of
-                Nothing ->
-                  let st' = scc' es st w
-                   in case min <$> lowlinkOf st' v <*> lowlinkOf st' w of
-                        Just min -> st' { lowlinkMap = Map.insert v min st'.lowlinkMap }
-                        _ -> st'
-                _ ->
-                  if w `elem` st.path
-                    then case min <$> lowlinkOf st v <*> indexOf st w of
-                           Just min -> st { lowlinkMap = Map.insert v min st.lowlinkMap }
-                           _ -> st
-                    else st
-                )
-   in if st2 `indexOf` v == st2 `lowlinkOf` v
-        then let newPath = popUntil v st2.path Nil
-               in st2 { components = newPath.component : st2.components
-                      , path = newPath.path
-                      }
-        else st2 
-scc' _ st _ = st
+updateLowLink :: forall v m. MonadState (SCCState v) m => Ord v => v -> v -> m Unit
+updateLowLink v w = do
+  vll <- lowLinkOf v
+  wll <- lowLinkOf w
+  flip traverse_ (min <$> vll <*> wll) $ \n -> do
+    modify_ (\st -> st { lowlinkMap = Map.insert v n st.lowlinkMap })
+
+altUpdateLowLink :: forall v m. MonadState (SCCState v) m => Ord v => v -> v -> m Unit
+altUpdateLowLink v w = do
+  vll <- lowLinkOf v
+  wll <- indexOf w
+  flip traverse_ (min <$> vll <*> wll) $ \n -> do
+    modify_ (\st -> st { lowlinkMap = Map.insert v n st.lowlinkMap })
+
+whenOnStack :: forall v m. MonadState (SCCState v) m => Ord v => v -> m Unit -> m Unit 
+whenOnStack v f = do
+  on <- gets (\st -> v `elem` st.path)
+  when on f
+
+whenIsRootNode :: forall v m. MonadState (SCCState v) m => Ord v => v -> m Unit -> m Unit 
+whenIsRootNode v f = do 
+  vll <- lowLinkOf v
+  vin <- indexOf v
+  when (vll == vin) f
+
+generateSCC :: forall v m. MonadState (SCCState v) m => Ord v => v -> m Unit
+generateSCC v = do
+  modify_ (\st -> let newPath = popUntil v st.path Nil
+                   in st { components = newPath.component : st.components
+                         , path = newPath.path
+                         })
 
 popUntil :: forall v. Eq v => v -> List v -> List v -> { path :: List v, component :: List v }
 popUntil _ Nil popped = { path: Nil, component: popped } 
